afe1721e57d671ce868c709ba5554d5cd0dcf08d
waigore <kelvin.ho.ka.wai@gmail.com>
2023-10-08 14:58:24 +0800

    Initial commit

---
+++ src/main/java/com/waigore/downdatube/DowndatubeApplication.java
@@ -0,0 +1,13 @@
+package com.waigore.downdatube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DowndatubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DowndatubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waigore/downdatube/DowndatubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waigore.downdatube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DowndatubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
